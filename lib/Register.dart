3a78d1bee90f32bf2ca48d7b925eeb97eb7c4327
Ho anh duong <hothianhduong16112002@hmail.com>
2023-05-17 22:42:50 +0700

    finish fist register

--- lib/Register.dart
+++ lib/Register.dart
@@ -1,8 +1,10 @@
+import 'dart:html';
 
 import 'package:cdio/Todolist_Color.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+
 class MRegister extends StatefulWidget {
   const MRegister({Key? key}) : super(key: key);
 
@@ -11,6 +13,11 @@ class MRegister extends StatefulWidget {
 }
 
 class _MRegisterState extends State<MRegister> {
+  final memail_contro = TextEditingController();
+  final mpass_confirm = TextEditingController();
+  final mpass = TextEditingController();
+  late  bool er_visible = false;
+  late  String text_error_register ="ư";
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -20,59 +27,96 @@ class _MRegisterState extends State<MRegister> {
           children: [
             Padding(
               padding: const EdgeInsets.all(30),
-              child: Text('Welcome to PLANPRO! ',style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold,color: Colors.black),),
+              child: Text(
+                'Welcome to PLANPRO! ',
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
             ),
             Padding(
               padding: const EdgeInsets.only(top: 10, bottom: 40),
-              child: Text('Let’s help to meet up your tasks.',style: TextStyle(fontSize: 13,color: Colors.black)),
+              child: Text('Let’s help to meet up your tasks.',
+                  style: TextStyle(fontSize: 13, color: Colors.black)),
             ),
-            _buildTextField('Enter your full name'),
-            _buildTextField('Enter your Email'),
-            _buildTextField('Enter Password'),
-            _buildTextField('Confirm password'),
+            _buildTextField('Enter your Email', memail_contro),
+            _buildTextField('Enter Password', mpass),
+            _buildTextField('Confirm password', mpass_confirm),
             Padding(
-              padding: EdgeInsets.only(left: 30,top: 10, right: 30, bottom: 10),
+              padding:
+                  EdgeInsets.only(left: 30, top: 10, right: 30, bottom: 10),
               child: SizedBox(
                 width: double.infinity,
                 height: 60,
                 child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(MColor.blue_main),
-                  ),
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(MColor.blue_main),
+                    ),
                     onPressed: () {
+                      if (mpass.text == mpass_confirm.text) {
+                        try {
+                          final credential = FirebaseAuth.instance
+                              .createUserWithEmailAndPassword(
+                            email: memail_contro.text,
+                            password: mpass.text,
+                          );
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'weak-password') {
+                            print('The password provided is too weak.');
+                            setState(() {
+                              er_visible = true;
+                              text_error_register='The password provided is too weak.';
+                              print('The password provided is too weak.');
+                            });
 
-                    }, child: Text('Register',style: TextStyle(fontSize: 18),)),
+                          } else if (e.code == 'email-already-in-use') {
+                            setState(() {
+                              er_visible = true;
+                              text_error_register ='The account already exists for that email.';
+                            });
+                          }
+                        } catch (e) {
+                          er_visible = true;
+                        }
+                      }else{
+                        setState(() {
+                          er_visible = true;
+                        });
+                      }
+                    },
+                    child: const Text(
+                      'Register',
+                      style: TextStyle(fontSize: 18),
+                    )),
               ),
             ),
-            Padding(padding: EdgeInsets.only(top: 10, bottom: 40),
+            Padding(
+              padding: const EdgeInsets.only(top: 10, bottom: 10),
               child: Center(
                 child: TextButton(
                   child: Row(
                     mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text('Already have an account ?',style: TextStyle(color: Colors.black,fontSize: 16),),
-                      Text('Sign In',style: TextStyle(color: MColor.blue_main,fontSize: 16),)
+                    children: const [
+                      Text(
+                        'Already have an account ?',
+                        style: TextStyle(color: Colors.black, fontSize: 16),
+                      ),
+                      Text(
+                        'Sign In',
+                        style: TextStyle(color: MColor.blue_main, fontSize: 16),
+                      )
                     ],
                   ),
-                  onPressed: () async {
-                    try {
-                      final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                        email: 'hothianhduong16112002@gmail.com',
-                        password: '123',
-                      );
-                    } on FirebaseAuthException catch (e) {
-                      if (e.code == 'weak-password') {
-                        print('The password provided is too weak.');
-                      } else if (e.code == 'email-already-in-use') {
-                        print('The account already exists for that email.');
-                      }
-                    } catch (e) {
-                      print(e);
-                    }
-                  },
+                  onPressed: () {},
                 ),
               ),
-            )
+            ),
+            Visibility(
+              visible: er_visible,
+              child: Text(text_error_register,style: TextStyle(color: Colors.red),),
+            ),
           ],
         ),
       ),
@@ -80,25 +124,23 @@ class _MRegisterState extends State<MRegister> {
   }
 }
 
-
-Widget _buildTextField(String text){
-  
+Widget _buildTextField(String text, TextEditingController mcontrol) {
   return Padding(
-    padding: EdgeInsets.only(left: 30,top: 10, right: 30, bottom: 10),
+    padding: EdgeInsets.only(left: 30, top: 10, right: 30, bottom: 10),
     child: TextField(
+      controller: mcontrol,
       decoration: InputDecoration(
           filled: true,
           fillColor: Colors.white,
-        border: OutlineInputBorder(
-          borderSide: BorderSide.none,
-          borderRadius: BorderRadius.all(Radius.circular(100))
-        ),
-        hintText: text,
-        hintStyle: TextStyle(fontSize: 13, fontFamily: 'Poppins')
-      ),
+          border: OutlineInputBorder(
+              borderSide: BorderSide.none,
+              borderRadius: BorderRadius.all(Radius.circular(100))),
+          hintText: text,
+          hintStyle: TextStyle(fontSize: 13, fontFamily: 'Poppins')),
     ),
   );
 }
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
