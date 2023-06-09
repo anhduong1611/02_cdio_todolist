3dc01bb85d1ba3d08196de2e8589941215f39790
Vũ Quang Nhật <101687608+vuquangnhat@users.noreply.github.com>
2023-06-09 13:30:19 +0700

    da sua xong

---
+++ lib/wigdet/item_task.dart
@@ -0,0 +1,38 @@
+import 'package:cdio/Todolist_Color.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/material.dart';
+class ItemsView extends StatelessWidget {
+  String date, name,color;
+
+   ItemsView({Key? key,required this.date,required this.name,required this.color}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 50,
+      decoration: BoxDecoration(
+        color : this.color == '1' ? MColor.blue_op: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(100))
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20,right: 20,top: 10,bottom: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(this.name,style: TextStyle(fontSize: 13),),
+            Row(
+              children: [
+                Checkbox(value: false, onChanged: (value) => true,),
+                SizedBox(width: 20,),
+                SvgPicture.asset('assets/icons/ic_edit.svg'),
+                SizedBox(width: 20,),
+                SvgPicture.asset('assets/icons/ic_bin.svg'),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
